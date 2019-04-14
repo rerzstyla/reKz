091a3c265d6c226de2e9e5056ac3a7ccffc590e5
reKz <rerzstyla@gmail.com>
2019-04-14 00:51:55 -0400

    Nuevo mayor_de_tres.py

---
+++ mayor_de_tres.py
@@ -0,0 +1,9 @@
+
+
+primer_numero = int(input('Primer numero: '))
+segundo_numero = int(input('Segundo numero: '))
+tercer_numero = int(input('Tercer numero: '))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print('El numero mas grande es {}'.format(resultado))
